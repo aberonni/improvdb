a0098c553929c130dc4c0ed049d57b4d1d1dc946
Domenico Gemoli <domenicogemoli@gmail.com>
2023-12-31 18:50:55 +0000

    Add category API

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,3 +1,4 @@
+import { categoryRouter } from "~/server/api/routers/category";
 import { resourceRouter } from "~/server/api/routers/resource";
 import { createTRPCRouter } from "~/server/api/trpc";
 
@@ -7,6 +8,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  category: categoryRouter,
   resource: resourceRouter,
 });
 
---
+++ src/server/api/routers/category.ts
@@ -0,0 +1,12 @@
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const categoryRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.db.category.findMany({
+      take: 100,
+      orderBy: {
+        name: "asc",
+      },
+    });
+  }),
+});
