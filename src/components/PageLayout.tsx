932edfad2b5efbc03d47fa2b235f4d263af67102
Domenico Gemoli <domenicogemoli@gmail.com>
2023-12-31 11:23:04 +0000

    Different page layout so that scrolling is preserved when navigating back and forth between index and single pages

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -3,16 +3,16 @@ import type { PropsWithChildren } from "react";
 export const PageLayout = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <div className="flex h-screen flex-col">
-        <header className="bg-white shadow">
+      <div className="h-full">
+        <header className="sticky left-0 top-0 z-10 bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">
               The Improvitory
             </h1>
           </div>
         </header>
-        <main className="grow overflow-auto">
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <main>
+          <div className="relative mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             {children}
           </div>
         </main>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,10 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { usePreserveScroll } from "~/hooks/usePreserveScroll";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  usePreserveScroll();
   return <Component {...pageProps} />;
 };
 
