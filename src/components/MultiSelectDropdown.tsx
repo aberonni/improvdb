9eba302bf471ba7c9e18d4e2d36b3d1086f992c5
Domenico Gemoli <domenicogemoli@gmail.com>
2024-01-04 22:34:37 +0100

    Style MultiSelectDropdown and fix bug with isClearable

--- src/components/MultiSelectDropdown.tsx
+++ src/components/MultiSelectDropdown.tsx
@@ -9,16 +9,15 @@ import type { Props } from "react-select";
 import CreatableSelect from "react-select/creatable";
 
 const controlStyles = {
-  base: "border rounded-lg bg-white hover:cursor-pointer",
-  focus: "border-primary-600 ring-1 ring-primary-500",
-  nonFocus: "border-gray-300 hover:border-gray-400",
+  base: "block rounded-md border-0 text-slate-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 border rounded-lg bg-white hover:cursor-pointer !min-h-0",
+  focus: "ring-2 ring-inset ring-indigo-600",
 };
-const placeholderStyles = "text-gray-500 pl-1 py-0.5";
-const selectInputStyles = "pl-1 py-0.5";
+const placeholderStyles = "text-gray-400 pl-2 py-0.5";
+const selectInputStyles = "pl-2 py-0.5";
 const valueContainerStyles = "p-1 gap-1";
-const singleValueStyles = "leading-7 ml-1";
+const singleValueStyles = "leading-7 ml-1 text-red-500";
 const multiValueStyles =
-  "bg-gray-100 rounded items-center py-0.5 pl-2 pr-1 gap-1.5";
+  "bg-gray-100 rounded items-center py-0 pl-2 pr-1 gap-1.5";
 const multiValueLabelStyles = "leading-6 py-0.5";
 const multiValueRemoveStyles =
   "border border-gray-200 bg-white hover:bg-red-50 hover:text-red-800 text-gray-500 hover:border-red-300 rounded-md";
@@ -31,7 +30,7 @@ const dropdownIndicatorStyles =
 const menuStyles = "p-1 mt-2 border border-gray-200 bg-white rounded-lg";
 const groupHeadingStyles = "ml-3 mt-2 mb-1 text-gray-500 text-sm";
 const optionStyles = {
-  base: "hover:cursor-pointer px-3 py-2 rounded",
+  base: "hover:cursor-pointer px-3 py-2 rounded !text-sm",
   focus: "bg-gray-100 active:bg-gray-200",
   selected:
     "after:content-['✔'] after:ml-2 after:text-green-500 text-gray-500",
@@ -48,21 +47,27 @@ interface Option {
   readonly value: string;
 }
 
-const createOption = (label: string) => ({
+const createOption = (label: string): Option => ({
   label,
   value: label,
 });
 
 export function MultiSelectDropown(props: MultiSelectDropdownProps) {
   const [inputValue, setInputValue] = useState("");
-  const [value, setValue] = useState<readonly Option[]>([]);
 
   const handleKeyDown: KeyboardEventHandler = (event) => {
     if (!inputValue) return;
     switch (event.key) {
       case "Enter":
       case "Tab":
-        setValue((prev) => [...prev, createOption(inputValue)]);
+        props.onChange?.(
+          [...(props.value as Option[]), createOption(inputValue)],
+          {
+            option: undefined,
+            removedValue: null,
+            action: "select-option",
+          },
+        );
         setInputValue("");
         event.preventDefault();
     }
@@ -81,68 +86,66 @@ export function MultiSelectDropown(props: MultiSelectDropdownProps) {
       inputValue: inputValue,
       isClearable: true,
       menuIsOpen: false,
-      onChange: (newValue: Option[]) => setValue(newValue),
       onInputChange: (newValue: string) => setInputValue(newValue),
       onKeyDown: handleKeyDown,
       placeholder: "Type something and press enter...",
-      value: value,
     };
   }
 
   return (
-    <TagType
-      isMulti
-      closeMenuOnSelect={false}
-      hideSelectedOptions={false}
-      unstyled
-      styles={{
-        input: (base) => ({
-          ...base,
-          "input:focus": {
-            boxShadow: "none",
-          },
-        }),
-        // On mobile, the label will truncate automatically, so we want to
-        // override that behaviour.
-        multiValueLabel: (base) => ({
-          ...base,
-          whiteSpace: "normal",
-          overflow: "visible",
-        }),
-        control: (base) => ({
-          ...base,
-          transition: "none",
-        }),
-      }}
-      classNames={{
-        control: ({ isFocused }) =>
-          clsx(
-            isFocused ? controlStyles.focus : controlStyles.nonFocus,
-            controlStyles.base,
-          ),
-        placeholder: () => placeholderStyles,
-        input: () => selectInputStyles,
-        valueContainer: () => valueContainerStyles,
-        singleValue: () => singleValueStyles,
-        multiValue: () => multiValueStyles,
-        multiValueLabel: () => multiValueLabelStyles,
-        multiValueRemove: () => multiValueRemoveStyles,
-        indicatorsContainer: () => indicatorsContainerStyles,
-        clearIndicator: () => clearIndicatorStyles,
-        indicatorSeparator: () => indicatorSeparatorStyles,
-        dropdownIndicator: () => dropdownIndicatorStyles,
-        menu: () => menuStyles,
-        groupHeading: () => groupHeadingStyles,
-        option: ({ isFocused, isSelected }) =>
-          clsx(
-            isFocused && optionStyles.focus,
-            isSelected && optionStyles.selected,
-            optionStyles.base,
-          ),
-        noOptionsMessage: () => noOptionsMessageStyles,
-      }}
-      {...props}
-      {...extraProps}
-    />
+    <div className="w-full rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+      <TagType
+        isMulti
+        closeMenuOnSelect={false}
+        hideSelectedOptions={false}
+        defaultMenuIsOpen
+        unstyled
+        styles={{
+          input: (base) => ({
+            ...base,
+            "input:focus": {
+              boxShadow: "none",
+            },
+          }),
+          // On mobile, the label will truncate automatically, so we want to
+          // override that behaviour.
+          multiValueLabel: (base) => ({
+            ...base,
+            whiteSpace: "normal",
+            overflow: "visible",
+          }),
+          control: (base) => ({
+            ...base,
+            transition: "none",
+          }),
+        }}
+        classNames={{
+          control: ({ isFocused }) =>
+            clsx(isFocused && controlStyles.focus, controlStyles.base),
+          placeholder: () => placeholderStyles,
+          input: () => selectInputStyles,
+          valueContainer: () => valueContainerStyles,
+          singleValue: () => singleValueStyles,
+          multiValue: () => multiValueStyles,
+          multiValueLabel: () => multiValueLabelStyles,
+          multiValueRemove: () => multiValueRemoveStyles,
+          indicatorsContainer: () => indicatorsContainerStyles,
+          clearIndicator: () => clearIndicatorStyles,
+          indicatorSeparator: () => indicatorSeparatorStyles,
+          dropdownIndicator: () => dropdownIndicatorStyles,
+          menu: () => menuStyles,
+          groupHeading: () => groupHeadingStyles,
+          option: ({ isFocused, isSelected }) =>
+            clsx(
+              isFocused && optionStyles.focus,
+              isSelected && optionStyles.selected,
+              optionStyles.base,
+            ),
+          noOptionsMessage: () => noOptionsMessageStyles,
+        }}
+        {...props}
+        {...extraProps}
+      />
+    </div>
   );
 }
