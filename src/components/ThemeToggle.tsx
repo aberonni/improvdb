109c4ec2e98b71b4ef7e858a256b8cc6613dc074
Domenico Gemoli <domenicogemoli@gmail.com>
2024-01-20 18:13:24 +0100

    suppressHydrationWarning on ThemeToggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -24,7 +24,7 @@ export function ThemeToggle({
       <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
-      <span className="ml-2 md:hidden">
+      <span className="ml-2 md:hidden" suppressHydrationWarning>
         {theme === "light" ? "Light" : "Dark"}
       </span>
     </Button>
