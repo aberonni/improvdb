c3cd8d26807ee903e04a42767667d5b88c7a0a36
Domenico Gemoli <domenicogemoli@gmail.com>
2024-01-29 11:11:30 +0100

    Adding PurgeCSS

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,15 @@ const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    "@fullhuman/postcss-purgecss": {
+      content: [
+        "./pages/**/*.{js,jsx,ts,tsx}",
+        "./components/**/*.{js,jsx,ts,tsx}",
+      ],
+      defaultExtractor: (/** @type {string} */ content) =>
+        content.match(/[\w-/:]+(?<!:)/g) ?? [],
+      safelist: ["html", "body"],
+    },
   },
 };
 
