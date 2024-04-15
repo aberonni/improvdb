50ce0b227df7754b4b7850f625904169c5c28420
Domenico Gemoli <domenicogemoli@gmail.com>
2024-04-15 11:10:50 +0200

    Fix new typescript errors and obsolete @ts-expect-error

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -79,7 +79,7 @@ export const authOptions: NextAuthOptions = {
     EmailProvider({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
+        port: parseInt(process.env.EMAIL_SERVER_PORT ?? "465"),
         auth: {
           user: process.env.EMAIL_SERVER_USER,
           pass: process.env.EMAIL_SERVER_PASSWORD,
