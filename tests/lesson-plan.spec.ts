3103368cc550004545d7111a0b960d5c3c2dbb19
Domenico Gemoli <domenicogemoli@gmail.com>
2024-12-26 12:31:21 +0100

    Fix E2E tests

--- tests/lesson-plan.spec.ts
+++ tests/lesson-plan.spec.ts
@@ -4,6 +4,6 @@ import { expectToMatchLightAndDarkThemesScreenshot } from "@/../tests/utils";
 
 test("looks good", async ({ page }) => {
   await page.goto("/lesson-plan/clrnyapk90001wqebn7it9zqo");
-  await expect(page).toHaveTitle(/Demo Lesson Plan - ImprovDB/);
+  await expect(page).toHaveTitle("Demo Lesson Plan - Lesson Plan on ImprovDB - Find improv games, exercises, and formats on ImprovDB - Improv games and lesson plans for teachers and students");
   await expectToMatchLightAndDarkThemesScreenshot(page, "demo-lesson-plan");
 });
--- tests/resource.spec.ts
+++ tests/resource.spec.ts
@@ -4,7 +4,7 @@ import { expectToMatchLightAndDarkThemesScreenshot } from "@/../tests/utils";
 
 test("looks good", async ({ page }) => {
   await page.goto("/resource/blue-ball");
-  await expect(page).toHaveTitle(/Blue Ball - ImprovDB/);
+  await expect(page).toHaveTitle("Blue Ball - 🚀 Warm-up / Exercise on ImprovDB - Find improv games, exercises, and formats on ImprovDB - Improv games and lesson plans for teachers and students");
   await expect(page.getByTestId("resource-favourite-button")).toBeEnabled({
     timeout: 15000,
   });
